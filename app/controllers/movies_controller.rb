55bfc66f608a34ab5bdbc9dd15d8a28cd0061139
Oliver Martell <olyver86@gmail.com>
2012-03-25 20:41:27 +0100

    Persist in session filters and ordering

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,13 +7,14 @@ class MoviesController < ApplicationController
   end
 
   def index
-    order_param  =  params[:sort_by] || "title"
-    order_param  += params[:order] == "desc" ? " DESC" : " ASC"
-    rating_param =  params[:ratings] || {}
-    @movies      =  Movie.order_by(order_param)
-    @movies      =  @movies.filter_on_ratings(rating_param.keys) if rating_param.keys.present?
-    rating_pairs =  Movie::ALL_RATINGS.zip([])
-    @all_ratings =  Hash[rating_pairs].merge(rating_param)
+    redirect_to movies_path(from_session) if from_session
+    movies_param = setup_sorting_filtering_params
+    order_clause = "#{movies_param[:sort_by]} #{movies_param[:order]}"
+    @movies      = Movie.order_by(order_clause)
+    @movies      = @movies.filter_on_ratings(movies_param[:ratings].keys) if movies_param[:ratings].keys.size > 0
+    rating_pairs = Movie::ALL_RATINGS.zip([])
+    @all_ratings = Hash[rating_pairs].merge(movies_param[:ratings])
+    session[:movies_param] = movies_param
   end
 
   def new
@@ -44,4 +45,18 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  private
+
+  def setup_sorting_filtering_params
+    sort_by      = params[:sort_by] || "title"
+    order_param  = params[:order]   || "asc"
+    rating_param = params[:ratings] || {}
+    { sort_by: sort_by, order: order_param , ratings: rating_param }
+  end
+
+  def from_session
+    if session[:movies_param] && !params[:ratings] && !params[:sort_by] && !params[:order]
+      session[:movies_param]
+    end
+  end
 end
